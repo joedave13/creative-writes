b2fd71a9ea7ddab6009bbe977a76ba05c0d67a7b
Joshua Davian <joshuadavian@gmail.com>
2022-10-27 13:53:38 +0700

    user signout & dashboard middleware

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -4,6 +4,7 @@ import { auth } from '../../utils/firebase';
 import { useRouter } from 'next/router';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 export default function Login() {
     const route = useRouter();
@@ -29,18 +30,23 @@ export default function Login() {
     }, [user]);
 
     return (
-        <div className='shadow-xl mt-32 p-10 text-gray-700 rounded-lg'>
-            <h2 className='text-2xl font-medium'>Join Today</h2>
-            <div className='py-4'>
-                <h3 className='py-4'>Sign in with one of the providers</h3>
-                <button
-                    onClick={googleLogin}
-                    className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'
-                >
-                    <FcGoogle className='text-2xl' />
-                    Sign In with Google
-                </button>
+        <>
+            <Head>
+                <title>Login Page | Creative Writes</title>
+            </Head>
+            <div className='shadow-xl mt-32 p-10 text-gray-700 rounded-lg'>
+                <h2 className='text-2xl font-medium'>Join Today</h2>
+                <div className='py-4'>
+                    <h3 className='py-4'>Sign in with one of the providers</h3>
+                    <button
+                        onClick={googleLogin}
+                        className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'
+                    >
+                        <FcGoogle className='text-2xl' />
+                        Sign In with Google
+                    </button>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
