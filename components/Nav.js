3283625d58410f604364caabe8cf349e61f539f8
Joshua Davian <joshuadavian@gmail.com>
2022-10-27 13:36:59 +0700

    user login with google firebase

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import { auth } from '../utils/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 export default function Nav() {
+    const [user, loading] = useAuthState(auth);
+    console.log(user);
+
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <nav className='flex justify-between items-center py-10'>
+            <Link href='/'>
+                <button className='text-lg font-medium'>Creative Writes</button>
+            </Link>
+            <ul className='flex items-center gap-10'>
+                {!user && (
+                    <Link href={'/auth/login'}>
+                        <a className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'>
+                            Join Now
+                        </a>
+                    </Link>
+                )}
+
+                {user && (
+                    <div className='flex items-center gap-6'>
+                        <Link href='/post'>
+                            <button className='font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm'>
+                                Post
+                            </button>
+                        </Link>
+                        <Link href='/dashboard'>
+                            <img
+                                className='w-12 rounded-full cursor-pointer'
+                                src={user.photoURL}
+                                alt='user-image-profile'
+                            />
+                        </Link>
+                    </div>
+                )}
+            </ul>
+        </nav>
     );
 }
