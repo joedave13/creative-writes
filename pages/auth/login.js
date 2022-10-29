01f561b57c75f54e693997224622055d3b870191
Joshua Davian <joshuadavian@gmail.com>
2022-10-29 19:30:04 +0700

    adding facebook login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,11 @@
 import { FcGoogle } from 'react-icons/fc';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { AiFillFacebook } from 'react-icons/ai';
+import {
+    signInWithPopup,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    updateProfile,
+} from 'firebase/auth';
 import { auth } from '../../utils/firebase';
 import { useRouter } from 'next/router';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -15,6 +21,24 @@ export default function Login() {
     const googleLogin = async () => {
         try {
             const result = await signInWithPopup(auth, googleProvider);
+            route.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const facebookProvider = new FacebookAuthProvider();
+
+    const facebookLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, facebookProvider);
+            const credential = await FacebookAuthProvider.credentialFromResult(
+                result
+            );
+            const token = credential.accessToken;
+            let photoUrl =
+                result.user.photoURL + '?height=500&access_token=' + token;
+            await updateProfile(auth.currentUser, { photoURL: photoUrl });
             route.push('/');
         } catch (error) {
             console.log(error);
@@ -38,12 +62,21 @@ export default function Login() {
                 <h2 className='text-2xl font-medium'>Join Today</h2>
                 <div className='py-4'>
                     <h3 className='py-4'>Sign in with one of the providers</h3>
+                </div>
+                <div className='flex flex-col gap-4'>
                     <button
                         onClick={googleLogin}
                         className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'
                     >
                         <FcGoogle className='text-2xl' />
                         Sign In with Google
+                    </button>
+                    <button
+                        onClick={facebookLogin}
+                        className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'
+                    >
+                        <AiFillFacebook className='text-2xl text-blue-400' />
+                        Sign In with Facebook
                     </button>
                 </div>
             </div>
